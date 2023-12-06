8767a2f16c5e96ac12a071d97d813b466775f8a0
Jossia Elang <jossia.soc@gmail.com>
2023-12-06 23:04:44 +0700

    Menambahkan fungsi kali

--- fungsi.js
+++ fungsi.js
@@ -1,5 +1,8 @@
 const tambah = (a, b) => a + b;
-console.log(tambah(1, 2));
+console.log(tambah(10, 5));
 
 const kurang = (a, b) => a - b;
-console.log(kurang(1, 2));
+console.log(kurang(10, 5));
+
+const kali = (a, b) => a * b;
+console.log(kali(10, 5));
