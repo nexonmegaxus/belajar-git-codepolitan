f08b681062b67c486aa278b4b5986398ff23c861
Jossia Elang <jossia.soc@gmail.com>
2023-12-06 23:20:35 +0700

    Merevisi fungsi kali dan bagi

--- fungsi.js
+++ fungsi.js
@@ -1,11 +1,11 @@
 const tambah = (a, b) => a + b;
-console.log(tambah(10, 5));
+console.log(tambah(1, 2));
 
 const kurang = (a, b) => a - b;
 console.log(kurang(10, 5));
 
-const kali = (a, b) => a * b;
-console.log(kali(10, 5));
+const perkalian = (a, b) => a * b;
+console.log(perkalian(10, 5));
 
-const bagi = (a, b) => a / b;
-console.log(bagi(10, 5));
+const pembagian = (a, b) => a / b;
+console.log(pembagian(10, 5));
